2ab881db2dcd1cf57c4ba01747c2975f026f527d
bansalapoorv29sb <112248370+bansalapoorv29sb@users.noreply.github.com>
2024-09-23 14:02:13 +0530

    Create Linear Regression.py

---
+++ Linear Regression.py
@@ -0,0 +1,6 @@
+#Supervised Learning
+'''
+Linear regression is a fundamental statistical method widely used in data science to explore relationships between variables. 
+In this, we will study through a hands-on project applying linear regression to a real-world dataset. 
+Our aim is to simplify the process and demonstrate how this technique can extract valuable insights from data in practical scenarios.
+'''
